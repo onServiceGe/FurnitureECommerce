504c78a8af446367a87a02879d8a9f8754200d1a
Avto <atukajiquridze@gmail.com>
2025-05-30 23:23:04 +0400

    feature: added folder structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,25 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "@/styles/globals.css";
+import localFont from "next/font/local";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const mainRegular = localFont({
+  variable: "--font-mainRegular",
+  src: "../public/assets/fonts/OnService-Regular.woff2",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const mainMedium = localFont({
+  variable: "--font-mainMedium",
+  src: "../public/assets/fonts/OnService-Medium.woff2",
+});
+
+const mainSemiBold = localFont({
+  variable: "--font-mainSemiBold",
+  src: "../public/assets/fonts/OnService-SemiBold.woff2",
+});
+
+const mainBold = localFont({
+  variable: "--font-mainBold",
+  src: "../public/assets/fonts/OnService-Bold.woff2",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +35,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${mainRegular.variable} ${mainMedium.variable} ${mainSemiBold.variable} ${mainBold.variable}`}
       >
         {children}
       </body>
